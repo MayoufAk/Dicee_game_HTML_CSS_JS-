8e80389100ac354aae8a387dece7853ea69acd1e
MayoufAk <102841858+MayoufAk@users.noreply.github.com>
2022-05-11 22:08:50 -0400

    Add files via upload

---
+++ dice/index.js
@@ -0,0 +1,20 @@
+var randonNumber1=Math.floor(Math.random()*6)+1;
+var reandomDiceImage="dice"+randonNumber1+ ".png";
+var randomImageSource="/images/"+reandomDiceImage;
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImageSource);
+
+var randonNumber2=Math.floor(Math.random()*6)+1;
+var reandomDiceImage="dice"+randonNumber2+ ".png";
+var randomImageSource2="/images/"+reandomDiceImage;
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImageSource2);
+ 
+
+if (randonNumber1> randonNumber2){
+    document.querySelector("h1").innerHTML="💤Player 1 Wins";
+}else if (randonNumber2> randonNumber1){
+    document.querySelector("h1").innerHTML="💯 Player2 wins";
+}else{
+    document.querySelector("h1").innerHTML="❌ Draw";
+}
